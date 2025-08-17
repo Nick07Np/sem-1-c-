8c32c274eddd4203ff5a88023879aacb54e5534d
Nick_07 <nikhilpawar0002@gmail.com>
2025-08-17 17:27:25 +0530

    Add files via upload

---
+++ addition of two numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=0, b=0;
+    cout << "Enter first number:";
+    cin >> a;
+    cout << "Enter second number:";
+    cin >> b;
+    cout << "The sum is: " << a+b << endl;
+}
